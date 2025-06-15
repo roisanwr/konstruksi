cfdf68a892d137bbe50de254e65a763f32a77ecc
roisanwr <roisanwar44@gmail.com>
2025-06-15 11:19:29 +0700

    logo

--- index.php
+++ index.php
@@ -61,9 +61,9 @@
     <header class="header fixed w-full z-50 py-4 px-6 lg:px-16">
         <div class="container mx-auto flex justify-between items-center">
             <div class="flex items-center">
-                <!-- <svg class="w-10 h-10 text-orange-600" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                </svg> -->
+                <svg class="w-10 h-10 text-orange-600" viewBox="0 0 24 24" fill="currentColor">
+                    <img src="/assets/img/azrina_logo.png" alt="Azrina Logo" class="w-full h-full">     
+                </svg>
                 <h1 class="ml-2 text-xl font-bold">Azrina</h1>
             </div>
             <nav class="hidden md:block">
