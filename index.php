396d9a008a8900f6d492e13227ef671ffe57f80a
roisanwr <roisanwar44@gmail.com>
2025-06-16 08:57:25 +0700

    haha

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <!-- Memanggil File CSS Eksternal (Path Diperbaiki dengan Cache Buster) -->
-    <link rel="stylesheet" href="assets/css/index.css?v=9">
+    <link rel="stylesheet" href="assets/css/index.css?v=10">
 </head>
 
 <body class="bg-white">
